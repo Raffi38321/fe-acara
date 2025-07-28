2cb179180f4e06e6585b3157f5f65ad2653ad133
Raffi <raffi38321@gmail.com>
2025-07-28 11:27:55 +0700

    feat: init project

---
+++ src/utils/cn.ts
@@ -0,0 +1,7 @@
+import { input } from "@nextui-org/react";
+import { ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(input));
+}
