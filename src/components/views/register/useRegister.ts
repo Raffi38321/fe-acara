9be351027cdccf415428d3681b3769dd1715bd0c
Raffi <raffi38321@gmail.com>
2025-07-29 19:05:06 +0700

    feat:registration logic

--- src/components/views/register/useRegister.ts
+++ src/components/views/register/useRegister.ts
@@ -1,6 +1,31 @@
 import { useState } from "react";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { IRegister } from "@/types/auth";
+import authServices from "@/services/auth";
+import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/router";
+
+const registerScehma = yup.object().shape({
+  fullName: yup.string().required("Please input your Full Name"),
+  userName: yup.string().required("Please input your User Name"),
+  email: yup
+    .string()
+    .email("Email format tidak valid")
+    .required("Please input your email"),
+  password: yup
+    .string()
+    .min(8, "Minimal 8 karakter")
+    .required("Input your Password"),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref("password"), ""], "Password not match")
+    .required("Input your Password confirmation"),
+});
 
 const useRegister = () => {
+  const router = useRouter();
   const [visiblePassword, setVisiblePassword] = useState({
     password: false,
     passwordConfirmation: false,
@@ -11,7 +36,45 @@ const useRegister = () => {
       [key]: !visiblePassword[key],
     });
   };
-  return { visiblePassword, hadleVisiblePassword };
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    reset,
+    setError,
+  } = useForm({
+    resolver: yupResolver(registerScehma),
+  });
+
+  const registerService = async (payload: IRegister) => {
+    const result = await authServices.register(payload);
+    return result;
+  };
+
+  const { mutate: mutateRegister, isPending: isPendingRegister } = useMutation({
+    mutationFn: registerService,
+    onError(error) {
+      setError("root", {
+        message: error.message,
+      });
+    },
+    onSuccess: () => {
+      router.push("/auth/register/success"), reset();
+    },
+  });
+
+  const handleRegister = (data: IRegister) => mutateRegister(data);
+
+  return {
+    visiblePassword,
+    hadleVisiblePassword,
+    control,
+    handleSubmit,
+    handleRegister,
+    isPendingRegister,
+    errors,
+  };
 };
 
 export default useRegister;
