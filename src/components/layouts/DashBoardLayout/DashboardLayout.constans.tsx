fa6c84051ec2e8b71676f3ac6da21d7b1f72be86
Raffi <raffi38321@gmail.com>
2025-08-03 12:12:46 +0700

    feat: ui dashboard

---
+++ src/components/layouts/DashBoardLayout/DashboardLayout.constans.tsx
@@ -0,0 +1,64 @@
+import {
+  CiBookmark,
+  CiGrid41,
+  CiSettings,
+  CiShoppingTag,
+  CiViewList,
+  CiWallet,
+} from "react-icons/ci";
+
+const SIDEBAR_MEMBER = [
+  {
+    key: "dashboard",
+    label: "Dashboard",
+    href: "/member",
+    icon: <CiGrid41 />,
+  },
+  {
+    key: "setting",
+    label: "Settting",
+    href: "/member/setting",
+    icon: <CiSettings />,
+  },
+  {
+    key: "transaction",
+    label: "Transaction",
+    href: "/member/transaction",
+    icon: <CiWallet />,
+  },
+];
+
+const SIDEBAR_ADMIN = [
+  {
+    key: "dashboard",
+    label: "Dashboard",
+    href: "/admin",
+    icon: <CiGrid41 />,
+  },
+  {
+    key: "Category",
+    label: "Category",
+    href: "/admin/category",
+    icon: <CiShoppingTag />,
+  },
+  {
+    key: "event",
+    label: "Event",
+    href: "/admin/event",
+    icon: <CiViewList />,
+  },
+  {
+    key: "banner",
+    label: "Banner",
+    href: "/admin/banner",
+    icon: <CiBookmark />,
+  },
+  {
+    key: "transaction",
+    label: "Transaction",
+    href: "/admin/transaction",
+    icon: <CiWallet />,
+  },
+];
+
+export { SIDEBAR_MEMBER, SIDEBAR_ADMIN };
---
+++ src/components/layouts/DashBoardLayout/DashboardLayout.tsx
@@ -0,0 +1,49 @@
+import PageHead from "@/components/commons/PageHead";
+import { ReactNode, useState } from "react";
+import DashboardLayoutSidebar from "./DashboardLayoutSidebar";
+import { SIDEBAR_ADMIN, SIDEBAR_MEMBER } from "./DashboardLayout.constans";
+import { Navbar, NavbarMenuToggle } from "@nextui-org/react";
+
+interface PropTypes {
+  title?: string;
+  children: ReactNode;
+  type?: string;
+  description?: string;
+}
+
+const DashboardLayout = (props: PropTypes) => {
+  const { children, title, type = "admin", description } = props;
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <PageHead title={title} />
+      <div className="max-w-screen-3xl 3xl:container flex">
+        <DashboardLayoutSidebar
+          sidebarItems={type === "admin" ? SIDEBAR_ADMIN : SIDEBAR_MEMBER}
+          isOpen={open}
+        />
+
+        <div className="h-screen w-full overflow-y-auto p-8">
+          <Navbar
+            className="flex justify-between bg-transparent px-0"
+            isBlurred={false}
+            position="static"
+            classNames={{ wrapper: "p-0" }}
+          >
+            <h1 className="text-3xl font-bold">{title}</h1>
+            <NavbarMenuToggle
+              aria-label={open ? "Close Menu" : "Open Menu"}
+              onClick={() => setOpen(!open)}
+              className="lg:hidden"
+            />
+          </Navbar>
+          <p className="mb-4 text-small">{description}</p>
+          {children}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DashboardLayout;
